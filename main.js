3bc778d295f2a0fcf3da7089c9a472167b7fc3ce
Hannah Papa <hmfloyd@gmail.com>
2022-12-12 09:41:40 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const{createMarble} = require("./marbleMaker.js")
+const marble = createMarble ("medium")
+console.log(marble)
+
+const{weaveBag} = require("./bagMaker.js")
+const bag = weaveBag("linen")
+console.log(bag)
